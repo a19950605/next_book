510bb9ee4f13389e24305f54e17c11be24f8d909
a19950605 <a1995060569@gmail.com>
2022-10-11 14:47:21 +0800

    first commit

---
+++ config/dbConnect.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const dbConnect = () => {
+    if(mongoose.connection.readyState >= 1){
+        return
+
+    }
+    
+    mongoose.connect(process.env.DB_LOCAL_URI, {
+        useNewUrlPraser : true,
+        useUnifiedTopology:true,
+        useFindAndModify:false,
+        useCreateIndex:true ,
+    })
+
+}
+
+
+export default  dbConnect;
