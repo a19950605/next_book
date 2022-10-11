9191913e1f20adc05caa64d67f69803b7fd7c96f
a19950605 <a1995060569@gmail.com>
2022-10-11 15:06:58 +0800

    updated room model

--- models/room.js
+++ models/room.js
@@ -102,7 +102,16 @@ const roomSchema = new mongoose.Schema({
                 required:true
             }
         }
-    ]
+    ],
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:'User',
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
 })
 
 export default mongoose.model.Room || mongoose.model('Room', roomSchema)
